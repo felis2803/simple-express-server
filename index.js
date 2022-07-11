b96910b096aa03682ff97baa93695426ea165c24
Ilya Afanasenko <fg1@mail.ru>
2022-07-11 12:40:08 +0400

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const path    = require('path');
+const express = require('express');
+
+const app  = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+    console.log(req.rawHeaders);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
